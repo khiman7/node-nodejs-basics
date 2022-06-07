a5422fb64c1e335fe41e84571f1de8dc395ccb09
Kyrylo Khiman <kyrylkhiman@gmail.com>
2022-06-07 17:24:27 +0300

    feat(cli): implement parseArgs and parseEnv functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,19 @@
 export const parseArgs = () => {
   const [executor, file, ...rest] = process.argv;
   const args = {};
-	
+
   rest.forEach((arg, index, array) => {
-	// TODO
+    if (arg.startsWith('--')) {
+      args[arg.slice(2)] = array[index + 1];
+    }
+  });
+
+  Object.entries(args).forEach((entry, index, array) => {
+    const [key, value] = entry;
+
+    process.stdout.write(`${key} is ${value}`);
+    process.stdout.write(`${index === array.length - 1 ? '' : ', '}`);
   });
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const variables = {};
+
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith('RSS_')) {
+      variables[key] = value;
+    }
+  }
+
+  Object.entries(variables).forEach((entry, index, array) => {
+    const [key, value] = entry;
+
+    process.stdout.write(`${key}=${value}`);
+    process.stdout.write(`${index === array.length - 1 ? '' : '; '}`);
+  });
 };
+
+parseEnv();
