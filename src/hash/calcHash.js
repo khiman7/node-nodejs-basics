0160e429c10c3bfc1d612226af281eaed21c6597
Kyrylo Khiman <kyrylkhiman@gmail.com>
2022-06-07 18:35:53 +0300

    feat(hash): implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { createHash } from 'crypto';
+import { readFileSync } from 'fs';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const buffer = await readFileSync('./files/fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+
+    hash.update(buffer);
+
+    return hash.digest('hex');
 };
+
+(async function () {
+  const hex = await calculateHash();
+
+  console.log(hex);
+})();
