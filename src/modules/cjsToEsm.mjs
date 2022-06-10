7662d12d67788899af560d9b822b0b230c21a337
Kyrylo Khiman <kyrylkhiman@gmail.com>
2022-06-10 11:38:43 +0300

    refactor(modules): remove IIFE

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,46 +1,45 @@
 import path from 'path';
-import url from 'url';
+import { fileURLToPath } from 'url';
 import { release, version } from 'os';
-import { createServer } from 'http';
+import { createServer as createServerHttp } from 'http';
 
 import './files/c.js';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
 
 const random = Math.random();
 
 let unknownObject;
 
-(async function () {
-  if (random > 0.5) {
-    unknownObject = await import('./files/a.json', {
-      assert: {
-        type: 'json',
-      },
-    });
-  } else {
-    unknownObject = await import('./files/b.json', {
-      assert: {
-        type: 'json',
-      },
-    });
-  }
-})();
+if (random > 0.5) {
+  unknownObject = await import('./files/a.json', {
+    assert: {
+      type: 'json',
+    },
+  });
+} else {
+  unknownObject = await import('./files/b.json', {
+    assert: {
+      type: 'json',
+    },
+  });
+}
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${url.fileURLToPath(import.meta.url)}`);
+console.log(`Path to current file is ${__filename}`);
 console.log(
-  `Path to current directory is ${url.fileURLToPath(
-    new URL('.', import.meta.url)
-  )}`
+  `Path to current directory is ${__dirname}`
 );
 
-const createMyServer = createServer((_, res) => {
+const createMyServer = createServerHttp((_, res) => {
   res.end('Request accepted');
 });
 
-export default {
+export {
   unknownObject,
   createMyServer,
 };
