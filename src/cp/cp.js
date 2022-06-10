95056da099ea83d81cd78ca05e7de2f99846ec44
Kyrylo Khiman <kyrylkhiman@gmail.com>
2022-06-10 10:55:24 +0300

    feat(cp): implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,12 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { join } from 'path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filepath = join(__dirname, 'files', 'script.js');
+  const child = fork(filepath, args);
 };
+
+spawnChildProcess(['test', 'argument', '123']);
